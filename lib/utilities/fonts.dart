ae5a8b221f1fb01992385c9873bd73d841b5105b
Dren <hilltechsolution@gmail.com>
2022-04-27 11:36:09 +0530

    Tic Tac Toe First Commit

---
+++ lib/utilities/fonts.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+var myTextStyle = TextStyle(color: Colors.white, fontSize: 30);
+
+var myNewFont = GoogleFonts.pressStart2p(
+    textStyle: const TextStyle(color: Colors.black, letterSpacing: 3));
+var myNewFontWhite = GoogleFonts.pressStart2p(
+    textStyle:
+        const TextStyle(color: Colors.white, letterSpacing: 3, fontSize: 15));
+var fontWhiteNumber = GoogleFonts.pressStart2p(
+    textStyle:
+        const TextStyle(color: Colors.white, letterSpacing: 3, fontSize: 25));
